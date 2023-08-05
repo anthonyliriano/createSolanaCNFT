3648653c6c4c55b71cac09023b822590f169fe4f
anthonyliriano <anthonylir@gmail.com>
2023-08-05 18:40:58 -0400

    builds on local :)

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
-import { Creator, MetadataArgs, TokenProgramVersion, TokenStandard, createMintToCollectionV1Instruction, PROGRAM_ID as BUBBLEGUM_PROGRAM_ID, createCreateTreeInstruction, } from "@metaplex-foundation/mpl-bubblegum";
-import { PROGRAM_ID as TOKEN_METADATA_PROGRAM_ID, createCreateMasterEditionV3Instruction, createCreateMetadataAccountV3Instruction, createSetCollectionSizeInstruction } from "@metaplex-foundation/mpl-token-metadata";
-import { AnchorProvider } from "@project-serum/anchor";
-import NodeWallet from "@project-serum/anchor/dist/cjs/nodewallet";
-import { SPL_ACCOUNT_COMPRESSION_PROGRAM_ID, SPL_NOOP_PROGRAM_ID, getConcurrentMerkleTreeAccountSize } from "@solana/spl-account-compression";
-import { TOKEN_PROGRAM_ID, createAssociatedTokenAccount, createMint, mintTo } from "@solana/spl-token";
-import { Connection, Keypair, PublicKey, SystemProgram, Transaction, sendAndConfirmTransaction } from "@solana/web3.js";
+import { Creator, MetadataArgs, TokenProgramVersion, TokenStandard, 
+    //  PROGRAM_ID as BUBBLEGUM_PROGRAM_ID, createMintToCollectionV1Instruction,  createCreateTreeInstruction, 
+    } from "@metaplex-foundation/mpl-bubblegum";
+// import { PROGRAM_ID as TOKEN_METADATA_PROGRAM_ID, createCreateMasterEditionV3Instruction, createCreateMetadataAccountV3Instruction, createSetCollectionSizeInstruction } from "@metaplex-foundation/mpl-token-metadata";
+// import { SPL_ACCOUNT_COMPRESSION_PROGRAM_ID, SPL_NOOP_PROGRAM_ID, getConcurrentMerkleTreeAccountSize } from "@solana/spl-account-compression";
+// import { TOKEN_PROGRAM_ID, createAssociatedTokenAccount, createMint, mintTo } from "@solana/spl-token";
+import { Keypair
+    // , PublicKey, SystemProgram, Transaction, sendAndConfirmTransaction 
+} from "@solana/web3.js";
 import { WrappedConnection } from "./ConnectionWrapper";
 
 
@@ -27,189 +29,189 @@ async function createCompressedNFT(name : string, symbol : string, creators: Cre
     };
 }
 
-async function setupTreeWithCompressedNFT(
-    connectionWrapper: WrappedConnection,
-    payerKeypair: Keypair,
-    compressedNFT: MetadataArgs,
-    maxDepth : number = 14,
-    maxBufferSize: number = 64 ) 
-{
-    const payer = payerKeypair.publicKey;
-    const merkleTreeKeypair = Keypair.generate();
-    const merkleTree = merkleTreeKeypair.publicKey;
-    const space = getConcurrentMerkleTreeAccountSize(maxDepth, maxBufferSize, 5);
+// async function setupTreeWithCompressedNFT(
+//     connectionWrapper: WrappedConnection,
+//     payerKeypair: Keypair,
+//     compressedNFT: MetadataArgs,
+//     maxDepth : number = 14,
+//     maxBufferSize: number = 64 ) 
+// {
+//     const payer = payerKeypair.publicKey;
+//     const merkleTreeKeypair = Keypair.generate();
+//     const merkleTree = merkleTreeKeypair.publicKey;
+//     const space = getConcurrentMerkleTreeAccountSize(maxDepth, maxBufferSize, 5);
 
-    const collectionMint = await createMint(
-        connectionWrapper,
-        payerKeypair,
-        payer,
-        payer,
-        0,
-        null,
-        null,
-        TOKEN_PROGRAM_ID
-    );
+//     const collectionMint = await createMint(
+//         connectionWrapper,
+//         payerKeypair,
+//         payer,
+//         payer,
+//         0,
+//         null,
+//         null,
+//         TOKEN_PROGRAM_ID
+//     );
     
-    // createAssociatedTokenAccount(connectionWrapper, payerKeypair, collectionMint, payer, null, null, null);
-    const collectionTokenAccount = await createAssociatedTokenAccount(connectionWrapper, payerKeypair, collectionMint, payer, null, null, null);//collectionMint.createAccount(payer)
-    await mintTo(connectionWrapper, payerKeypair, collectionMint, payer, payer, 1);    
-    // await collectionMint.MintTo(collectionTokenAccount, payerKeypair, [], 1);
-    const [collectionMetadataAccount, _b] = PublicKey.findProgramAddressSync(
-        [
-            Buffer.from("metadata", "utf8"),
-            TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-            collectionMint.toBuffer()
-        ],
-        TOKEN_METADATA_PROGRAM_ID
-    );
+//     // createAssociatedTokenAccount(connectionWrapper, payerKeypair, collectionMint, payer, null, null, null);
+//     const collectionTokenAccount = await createAssociatedTokenAccount(connectionWrapper, payerKeypair, collectionMint, payer, null, null, null);//collectionMint.createAccount(payer)
+//     await mintTo(connectionWrapper, payerKeypair, collectionMint, payer, payer, 1);    
+//     // await collectionMint.MintTo(collectionTokenAccount, payerKeypair, [], 1);
+//     const [collectionMetadataAccount, _b] = PublicKey.findProgramAddressSync(
+//         [
+//             Buffer.from("metadata", "utf8"),
+//             TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+//             collectionMint.toBuffer()
+//         ],
+//         TOKEN_METADATA_PROGRAM_ID
+//     );
 
-    const collectionMetadataIX = createCreateMetadataAccountV3Instruction(
-        {
-            metadata: collectionMetadataAccount,
-            mint: collectionMint,
-            mintAuthority: payer,
-            payer,
-            updateAuthority: payer
-        },
-        {
-            createMetadataAccountArgsV3: {
-                data: {
-                    name: "erm",
-                    symbol: "erm",
-                    uri: "erm",
-                    sellerFeeBasisPoints: 100,
-                    creators: null,
-                    collection: null,
-                    uses: null
-                },
-                isMutable: false,
-                collectionDetails: null
-            }
-        }
-    );
+//     const collectionMetadataIX = createCreateMetadataAccountV3Instruction(
+//         {
+//             metadata: collectionMetadataAccount,
+//             mint: collectionMint,
+//             mintAuthority: payer,
+//             payer,
+//             updateAuthority: payer
+//         },
+//         {
+//             createMetadataAccountArgsV3: {
+//                 data: {
+//                     name: "erm",
+//                     symbol: "erm",
+//                     uri: "erm",
+//                     sellerFeeBasisPoints: 100,
+//                     creators: null,
+//                     collection: null,
+//                     uses: null
+//                 },
+//                 isMutable: false,
+//                 collectionDetails: null
+//             }
+//         }
+//     );
     
-    const [collectionMasterEditionAccount, _b2] = await PublicKey.findProgramAddress(
-        [
-            Buffer.from("metadata", "utf8"),
-            TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-            collectionMint.toBuffer(),
-            Buffer.from("edition", "utf8")
-        ],
-        TOKEN_METADATA_PROGRAM_ID
-    );
+//     const [collectionMasterEditionAccount, _b2] = await PublicKey.findProgramAddress(
+//         [
+//             Buffer.from("metadata", "utf8"),
+//             TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+//             collectionMint.toBuffer(),
+//             Buffer.from("edition", "utf8")
+//         ],
+//         TOKEN_METADATA_PROGRAM_ID
+//     );
 
-    const collectionMasterEditionIX = createCreateMasterEditionV3Instruction(
-        {
-            edition: collectionMasterEditionAccount,
-            mint: collectionMint,
-            updateAuthority: payer,
-            mintAuthority: payer,
-            payer: payer,
-            metadata: collectionMetadataAccount
-        },
-        {
-            createMasterEditionArgs: {
-                maxSupply: 0
-            }
-        }
-    );
+//     const collectionMasterEditionIX = createCreateMasterEditionV3Instruction(
+//         {
+//             edition: collectionMasterEditionAccount,
+//             mint: collectionMint,
+//             updateAuthority: payer,
+//             mintAuthority: payer,
+//             payer: payer,
+//             metadata: collectionMetadataAccount
+//         },
+//         {
+//             createMasterEditionArgs: {
+//                 maxSupply: 0
+//             }
+//         }
+//     );
 
-    const sizeCollectionIX = createSetCollectionSizeInstruction(
-        {
-            collectionMetadata: collectionMetadataAccount,
-            collectionAuthority: payer,
-            collectionMint: collectionMint
-        },
-        {
-            setCollectionSizeArgs: { size: 0}
-        }
-    );
+//     const sizeCollectionIX = createSetCollectionSizeInstruction(
+//         {
+//             collectionMetadata: collectionMetadataAccount,
+//             collectionAuthority: payer,
+//             collectionMint: collectionMint
+//         },
+//         {
+//             setCollectionSizeArgs: { size: 0}
+//         }
+//     );
 
-    let tx_collection = new Transaction()
-        .add(collectionMetadataIX)
-        .add(collectionMasterEditionIX)
-        .add(sizeCollectionIX);
+//     let tx_collection = new Transaction()
+//         .add(collectionMetadataIX)
+//         .add(collectionMasterEditionIX)
+//         .add(sizeCollectionIX);
 
-    tx_collection.feePayer = payer;
-    await sendAndConfirmTransaction(
-        connectionWrapper,
-        tx_collection,
-        [payerKeypair],
-        {
-            commitment: "confirmed",
-            skipPreflight: true
-        }
-    );
+//     tx_collection.feePayer = payer;
+//     await sendAndConfirmTransaction(
+//         connectionWrapper,
+//         tx_collection,
+//         [payerKeypair],
+//         {
+//             commitment: "confirmed",
+//             skipPreflight: true
+//         }
+//     );
 
-    const allocTreeIx = SystemProgram.createAccount({
-        fromPubkey: payer,
-        newAccountPubkey: merkleTree,
-        lamports: await connectionWrapper.getMinimumBalanceForRentExemption(space),
-        space: space,
-        programId: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID
-    });
-    const [treeAuthority, _bump] = await PublicKey.findProgramAddress(
-        [merkleTree.toBuffer()],
-        BUBBLEGUM_PROGRAM_ID
-    );
-    const createTreeIx = createCreateTreeInstruction(
-        {
-            merkleTree,
-            treeAuthority,
-            treeCreator: payer,
-            payer,
-            logWrapper: SPL_NOOP_PROGRAM_ID,
-            compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID
-        },
-        {
-            maxBufferSize,
-            maxDepth,
-            public: false
-        },
-        BUBBLEGUM_PROGRAM_ID
-    );
-    const [bgumSigner, __] = await PublicKey.findProgramAddress(
-        [Buffer.from("collection_cpi", "utf8")],
-        BUBBLEGUM_PROGRAM_ID
-    );
-    const mintIx = createMintToCollectionV1Instruction(
-        {
-            merkleTree,
-            treeAuthority,
-            treeDelegate: payer,
-            payer,
-            leafDelegate: payer,
-            leafOwner: payer,
-            compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
-            logWrapper: SPL_NOOP_PROGRAM_ID,
-            collectionAuthority: payer,
-            collectionAuthorityRecordPda: BUBBLEGUM_PROGRAM_ID,
-            collectionMint: collectionMint,
-            collectionMetadata: collectionMetadataAccount,
-            editionAccount: collectionMasterEditionAccount,
-            bubblegumSigner: bgumSigner,
-            tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID
-        },
-        {
-            metadataArgs: Object.assign(compressedNFT, {
-                collection: {key: collectionMint, verified: false}
-            })
-        }
-    );
-    let tx = new Transaction().add(allocTreeIx).add(createTreeIx);
-    tx.feePayer = payer;
-    await sendAndConfirmTransaction(
-        connectionWrapper,
-        tx,
-        [merkleTreeKeypair, payerKeypair],
-        {
-            commitment: "confirmed",
-            skipPreflight: true
-        });
-        return {
-            merkleTree
-        };
-};
+//     const allocTreeIx = SystemProgram.createAccount({
+//         fromPubkey: payer,
+//         newAccountPubkey: merkleTree,
+//         lamports: await connectionWrapper.getMinimumBalanceForRentExemption(space),
+//         space: space,
+//         programId: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID
+//     });
+//     const [treeAuthority, _bump] = await PublicKey.findProgramAddress(
+//         [merkleTree.toBuffer()],
+//         BUBBLEGUM_PROGRAM_ID
+//     );
+//     const createTreeIx = createCreateTreeInstruction(
+//         {
+//             merkleTree,
+//             treeAuthority,
+//             treeCreator: payer,
+//             payer,
+//             logWrapper: SPL_NOOP_PROGRAM_ID,
+//             compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID
+//         },
+//         {
+//             maxBufferSize,
+//             maxDepth,
+//             public: false
+//         },
+//         BUBBLEGUM_PROGRAM_ID
+//     );
+//     const [bgumSigner, __] = await PublicKey.findProgramAddress(
+//         [Buffer.from("collection_cpi", "utf8")],
+//         BUBBLEGUM_PROGRAM_ID
+//     );
+//     const mintIx = createMintToCollectionV1Instruction(
+//         {
+//             merkleTree,
+//             treeAuthority,
+//             treeDelegate: payer,
+//             payer,
+//             leafDelegate: payer,
+//             leafOwner: payer,
+//             compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
+//             logWrapper: SPL_NOOP_PROGRAM_ID,
+//             collectionAuthority: payer,
+//             collectionAuthorityRecordPda: BUBBLEGUM_PROGRAM_ID,
+//             collectionMint: collectionMint,
+//             collectionMetadata: collectionMetadataAccount,
+//             editionAccount: collectionMasterEditionAccount,
+//             bubblegumSigner: bgumSigner,
+//             tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID
+//         },
+//         {
+//             metadataArgs: Object.assign(compressedNFT, {
+//                 collection: {key: collectionMint, verified: false}
+//             })
+//         }
+//     );
+//     let tx = new Transaction().add(allocTreeIx).add(createTreeIx);
+//     tx.feePayer = payer;
+//     await sendAndConfirmTransaction(
+//         connectionWrapper,
+//         tx,
+//         [merkleTreeKeypair, payerKeypair],
+//         {
+//             commitment: "confirmed",
+//             skipPreflight: true
+//         });
+//         return {
+//             merkleTree
+//         };
+// };
 
 function main() {
     const rpcUrl = "https://rpc-devnet.aws.metaplex.com/";
